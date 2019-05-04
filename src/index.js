d61c49472910a9580d43625eaeb456e3d1136cf4
camila93 <camila.ap.93@gmail.com>
2019-05-03 21:05:53 -0300

    Vue SSR

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import Vue from 'vue'
-import App from './app.vue'
-
-new Vue({
-    el: '#app',
-    render(h) {
-        return h(App)
-    }
+const Vue = require('vue')
+const server = require('express')()
+const renderer = require('vue-server-renderer').createRenderer({
+    template: require('fs').readFileSync('./index.html', 'utf-8')
 })
 
+server.get('*', (req, res) => {
+    const context = {
+        title: 'Titulo da página'
+    }
+
+    const app = new Vue({
+        data: {
+            url: req.url
+        },
+        template: `<div>The visited URL is: {{ url }}</div>`
+    })
+    
+    renderer.renderToString(app, context, (err, html) => {
+        res.send(html);
+    })
+})
+
+server.listen(8080)
